c12f0b30167f9fe33bc1818c2ab75f9ee20b2288
James Laird-Wah <james@laird-wah.net>
2018-01-23 00:12:51 +1100

    set Linux tap interface up on start
    On some, but not all Linux machines, the tap interface doesn't start out
    up, and then it won't accept writes (obviously).
    
    Since both BSD and Linux interfaces now have more in common, merge the
    common parts into a parent class.

--- tun.py
+++ tun.py
@@ -17,28 +17,15 @@ class ifreq(ctypes.Structure):
         ('payload', ifreq_payload),
     ]
 
-class LinuxTAP(object):
-    TUNSETIFF = 0x400454CA
-    IFF_TAP = 0x0002
-    IFF_NO_PI = 0x1000
-    SIOCSIFMTU = 0x8922
-
-    def __init__(self, mtu):
-        self.fd = os.open('/dev/net/tun', os.O_RDWR)
-
-        ifr = ifreq()
-        ifr.payload.ifr_flags = self.IFF_TAP | self.IFF_NO_PI
-        fcntl.ioctl(self.fd, self.TUNSETIFF, ifr)
-        self.iface = ifr.ifr_name
-
-        # you can't set the MTU on the tun interface in Linux? herf
+class TAP(object):
+    def set_mtu_up(self, ifr, mtu):
         ifr.payload.ifr_mtu = mtu
-        s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+        s = socket.socket(self.CTL_AF, socket.SOCK_RAW)
         fcntl.ioctl(s, self.SIOCSIFMTU, ifr)
+        fcntl.ioctl(s, self.SIOCGIFFLAGS, ifr)
+        ifr.payload.ifr_flags |= self.IFF_UP
+        fcntl.ioctl(s, self.SIOCSIFFLAGS, ifr)
         s.close()
-
-    def close(self):
-        os.close(self.fd)
 
     def fileno(self):
         return self.fd
@@ -49,13 +36,37 @@ class LinuxTAP(object):
     def read(self):
         return os.read(self.fd, 8192)
 
-class BSDTAP(object):
+class LinuxTAP(TAP):
+    TUNSETIFF = 0x400454CA
+    IFF_TAP = 0x0002
+    IFF_NO_PI = 0x1000
+    SIOCSIFMTU = 0x8922
+    SIOCSIFFLAGS = 0x8914
+    SIOCGIFFLAGS = 0x8913
+    IFF_UP = 1
+    CTL_AF = socket.AF_PACKET
+
+    def __init__(self, mtu):
+        self.fd = os.open('/dev/net/tun', os.O_RDWR)
+
+        ifr = ifreq()
+        ifr.payload.ifr_flags = self.IFF_TAP | self.IFF_NO_PI
+        fcntl.ioctl(self.fd, self.TUNSETIFF, ifr)
+        self.iface = ifr.ifr_name
+
+        self.set_mtu_up(ifr, mtu)
+
+    def close(self):
+        os.close(self.fd)
+
+class BSDTAP(TAP):
     TAPGIFNAME = 0x4020745d
     SIOCIFDESTROY = 0x80206979
     SIOCSIFFLAGS = 0x80206910
     SIOCGIFFLAGS = 0xc0206911
     SIOCSIFMTU = 0x80206934
     IFF_UP = 1
+    CTL_AF = socket.AF_INET
 
     def __init__(self, mtu):
         self.fd = os.open('/dev/tap', os.O_RDWR)
@@ -67,16 +78,7 @@ class BSDTAP(object):
         # BSD TAP interfaces have to be explicitly destroyed
         atexit.register(self.close)
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_RAW)
-
-        fcntl.ioctl(s, self.SIOCGIFFLAGS, ifr)
-        ifr.payload.ifr_flags |= self.IFF_UP
-        fcntl.ioctl(s, self.SIOCSIFFLAGS, ifr)
-
-        ifr.payload.ifr_mtu = mtu
-        fcntl.ioctl(s, self.SIOCSIFMTU, ifr)
-
-        s.close()
+        self.set_mtu_up(ifr, mtu)
 
     def close(self):
         os.close(self.fd)
@@ -85,15 +87,6 @@ class BSDTAP(object):
         s = socket.socket(socket.AF_INET, socket.SOCK_RAW)
         fcntl.ioctl(s, self.SIOCIFDESTROY, ifr)
         s.close()
-
-    def fileno(self):
-        return self.fd
-
-    def write(self, data):
-        os.write(self.fd, data)
-
-    def read(self):
-        return os.read(self.fd, 8192)
 
 def mktap(mtu=1280):
     if sys.platform.startswith('linux'):
