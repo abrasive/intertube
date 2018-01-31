5039a4708d3a48a93ad3ca3e70ebf1399d26c115
James Laird-Wah <james@laird-wah.net>
2018-02-01 10:37:37 +1100

    Update the remote IP when connection is lost
    When one end of the link is eg. a consumer internet connection, dynamic
    DNS can be used to automatically get the new IP.
    It's not clear under what circumstances a fresh request is actually
    issued, though, so in a fit of paranoia this patch uses dnspython to
    send the request directly to the upstream nameserver.
    
    This is invoked whenever the connection seems to be down ie. we get an
    ICMP Unreachable response (hence a socket error) or we haven't received
    a packet in a while (30s, while the keepalives are at 10s interval). DNS
    queries are rate limited to avoid rude behaviour.

--- pipe.py
+++ pipe.py
@@ -1,5 +1,8 @@
 import socket
 import nacl.secret
+import dns.resolver
+import re
+import time
 
 class SecretPipe(object):
     @classmethod
@@ -11,24 +14,52 @@ class SecretPipe(object):
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(("0.0.0.0", lport))
+        self.rhost = rhost
+        self.rport = rport
         self.peer = (rhost, rport)
-        self.sock.connect(self.peer)
+        self.last_dns = 0
+        self.use_dns = not re.match('^(\d+\.){3}\d+$', rhost)
+        self.last_rx = 0
+        self.reconnect()
 
     def send(self, data):
         crypted = self.box.encrypt(data)
         try:
             self.sock.send(crypted)
         except socket.error:
-            self.sock.connect(self.peer)
+            self.reconnect()
+
+        if time.time() > self.last_rx + 30:
+            self.last_rx = time.time()
+            self.reconnect()
 
     def recv(self):
+        self.last_rx = time.time()
         try:
             return self.box.decrypt(self.sock.recv(8192))
         except (nacl.exceptions.ValueError, nacl.exceptions.CryptoError):
             print "Bad packet received (wrong key?)"
             return
         except socket.error:
-            self.sock.connect(self.peer)
+            self.reconnect()
 
     def fileno(self):
         return self.sock.fileno()
+
+    def query_dns(self):
+        self.last_dns = time.time()
+        try:
+            ipaddr = dns.resolver.query(self.rhost, 'A')[0].address
+        except Exception, e:
+            print e
+            return
+
+        if ipaddr != self.peer[0]:
+            print "New peer address found: %s" % ipaddr
+            self.peer = (ipaddr, self.rport)
+
+    def reconnect(self):
+        if self.use_dns and time.time() > (self.last_dns + 60):
+            self.query_dns()
+
+        self.sock.connect(self.peer)
